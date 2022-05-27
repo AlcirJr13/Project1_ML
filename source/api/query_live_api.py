ef9229fbf3b7882f551e9d59cc90aaa507731289
Alcir Jr <alcir_jr13@hotmail.com>
2022-05-27 18:08:11 -0300

    teste 2 hroku

--- source/api/query_live_api.py
+++ source/api/query_live_api.py
@@ -33,30 +33,30 @@ person = {
     }
 
 #url = "http://127.0.0.1:8000"
-url = "https://teste-am.herokuapp.com"
+url = "https://project1-ml-ppgeec1.herokuapp.com/"
 response = requests.post(f"{url}/predict",
                          json=person)
 
 
 print(f"Request: {url}/predict")
-#print(f"Person: \n customerID: {person['customerID']}\n gender: {person['gender']}\n"\
-#      f" SeniorCitizen: {person['SeniorCitizen']}\n Partner: {person['Partner']}\n"\
-#      f" Dependents: {person['Dependents']}\n"\
-#      f" tenure: {person['tenure']}\n"\
-#      f" PhoneService: {person['PhoneService']}\n"\
-#      f" MultipleLines: {person['MultipleLines']}\n"\
-#      f" InternetService: {person['InternetService']}\n"\
-#      f" OnlineSecurity: {person['OnlineSecurity']}\n"\
-#      f" OnlineBackup: {person['OnlineBackup']}\n"\
-#      f" DeviceProtection: {person['DeviceProtection']}\n"\
-#      f" TechSupport: {person['TechSupport']}\n"\
-#      f" StreamingTV: {person['StreamingTV']}\n"\
-#      f" StreamingMovies: {person['StreamingMovies']}\n"\
-#      f" Contract: {person['Contract']}\n"\
-#      f" PaperlessBilling: {person['PaperlessBilling']}\n"\
-#      f" PaymentMethod: {person['PaymentMethod']}\n"\
-#      f" MonthlyCharges: {person['MonthlyCharges']}\n"\
-#      f" TotalCharges: {person['TotalCharges']}\n"
-#     )
+print(f"Person: \n customerID: {person['customerID']}\n gender: {person['gender']}\n"\
+      f" SeniorCitizen: {person['SeniorCitizen']}\n Partner: {person['Partner']}\n"\
+      f" Dependents: {person['Dependents']}\n"\
+      f" tenure: {person['tenure']}\n"\
+      f" PhoneService: {person['PhoneService']}\n"\
+      f" MultipleLines: {person['MultipleLines']}\n"\
+      f" InternetService: {person['InternetService']}\n"\
+      f" OnlineSecurity: {person['OnlineSecurity']}\n"\
+      f" OnlineBackup: {person['OnlineBackup']}\n"\
+      f" DeviceProtection: {person['DeviceProtection']}\n"\
+      f" TechSupport: {person['TechSupport']}\n"\
+      f" StreamingTV: {person['StreamingTV']}\n"\
+      f" StreamingMovies: {person['StreamingMovies']}\n"\
+      f" Contract: {person['Contract']}\n"\
+      f" PaperlessBilling: {person['PaperlessBilling']}\n"\
+      f" PaymentMethod: {person['PaymentMethod']}\n"\
+      f" MonthlyCharges: {person['MonthlyCharges']}\n"\
+      f" TotalCharges: {person['TotalCharges']}\n"
+     )
 print(f"Result of model inference: {response.json()}")
 print(f"Status code: {response.status_code}")
